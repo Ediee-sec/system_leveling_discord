e422ebae4b626d05fed38436b14f82b5c50c6882
Ediee-sec <emersonrox8@gmail.com>
2024-09-22 12:23:28 -0300

    adjust timezone

--- log/logger.py
+++ log/logger.py
@@ -1,8 +1,12 @@
 from db import insert_log
-from datetime import timedelta
+from datetime import datetime, timedelta
 
-def get_data_by_user(time,user,type,xp,multiplier,xp_multiplied,level,channel, content):
-    time = time - timedelta(hours=3)
+def get_data_by_user(time, user, type, xp, multiplier, xp_multiplied, level, channel, content):
+    # Supondo que 'time' esteja em um formato como 'YYYY-MM-DD HH:MM:SS'
+    time_format = '%Y-%m-%d %H:%M:%S'  # Altere este formato conforme a estrutura de sua string de tempo
+    time = datetime.strptime(time, time_format)  # Converte a string 'time' para um objeto datetime
+    time = time - timedelta(hours=3)  # Subtrai 3 horas do objeto datetime
+    
     dicionarie = {
         'datetime': time,
         'user': user,
@@ -16,6 +20,7 @@ def get_data_by_user(time,user,type,xp,multiplier,xp_multiplied,level,channel, c
     }
     
     insert_log.insert_user_activity(dicionarie)
+
     
     
     
